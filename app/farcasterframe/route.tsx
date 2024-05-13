a187a88b72b36573d401585089f3e51c832c632d
victorwick <victor.dsdevs@gmail.com>
2024-05-13 22:43:31 +0700

    fixing subscribing frames

--- app/farcasterframe/route.tsx
+++ app/farcasterframe/route.tsx
@@ -5,10 +5,26 @@ import { Button } from "frames.js/next";
 const frameHandler = frames(async () => {
   return {
     image: (
-      <div tw="flex flex-col justify-center bg-[#17101F] w-full h-full">
-        <p tw="text-white w-min mx-auto">
-          Welcome, this is a testing page to Subscribe Alfafrens Channel
+      <div tw="flex flex-col justify-center bg-[#0200de] w-full h-full">
+        <img
+          tw="w-44 h-44  rounded-full mx-auto"
+          src="https://imagedelivery.net/BXluQx4ige9GuW0Ia56BHw/cab32c8c-2a0a-450e-1053-6daf33990600/rectcrop3"
+        ></img>
+        <p tw="font-bold text-center text-white w-min mx-auto text-4xl">
+          VictorWick 🎩
         </p>
+        <div tw="flex">
+          <p tw="font-bold text-center text-white w-min mx-auto text-3xl">
+            1244 Subscriber
+          </p>
+
+          <p tw="font-bold text-white w-min mx-auto text-3xl">473 Staker</p>
+        </div>
+        <div tw="flex bg-[#8afa51] ">
+          <p tw="text-[#0200de] text-2xl text-center mx-auto">
+            *By Subscribing you'll get EST. 130 ALFA Daily
+          </p>
+        </div>
       </div>
     ),
     buttons: [
